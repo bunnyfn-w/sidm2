92f4218f6af1d1d870d8f9f21538556589b9d238
bunnyfn-w <bonakornel@gmail.com>
2026-03-26 14:29:50 +0100

    Update sidm.bat

--- sidm.bat
+++ sidm.bat
@@ -4,12 +4,14 @@ title SteamIDMaker - lilbona
 color 5
 
 echo.
-echo.   88     88 88     88""Yb  dP"Yb  88b 88    db    
-echo.   88     88 88     88__dP dP   Yb 88Yb88   dPYb   
-echo.   88  .o 88 88  .o 88""Yb Yb   dP 88 Y88  dP__Yb  
-echo.   88ood8 88 88ood8 88oodP  YbodP  88  Y8 dP""""Yb 
-echo.   Enter Steam game id
-echo.   Use /update for updates 
+echo.        88     88 88     88""Yb  dP"Yb  88b 88    db    
+echo.        88     88 88     88__dP dP   Yb 88Yb88   dPYb   
+echo.        88  .o 88 88  .o 88""Yb Yb   dP 88 Y88  dP__Yb  
+echo.        88ood8 88 88ood8 88oodP  YbodP  88  Y8 dP""""Yb 
+
+
+echo.             Enter Steam game id
+echo.             Use /update for updates 
 
 :askAppID
 echo.
